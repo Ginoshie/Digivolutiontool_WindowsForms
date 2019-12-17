e8e70984ef67e3b83a68ff3b3ea6e2e293b87d17
Ginoshie <ginoshie@gmail.com>
2019-12-17 21:01:29 +0100

    GH-#2 Reworked DigimonStats
    Created a DigimonCombatStats object to make it possible to use a Toolbox class for evolution determination. Now it is possible to pass all CombatStat related criteria to a method using a single object.

---
+++ DigimonWorldTools_WindowsForms/EvolutionTool/DigimonCombatStats.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigimonWorldTools_WindowsForms.EvolutionTool
+{
+    public class DigimonCombatStats
+    {
+        public int HP { get; set; }
+
+        public int MP { get; set; }
+
+        public int Off { get; set; }
+
+        public int Def { get; set; }
+
+        public int Speed { get; set; }
+
+        public int Brains { get; set; }
+    }
+}
